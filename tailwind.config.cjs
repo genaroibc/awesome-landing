607a40084886760ae6e10283f3ae6c6dda86d2cd
genaroibc <bonavitaismael@gmail.com>
2023-09-25 18:08:36 -0300

    feat: add tailwind animation and global styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fade-in 0.2s ease-in'
+      }
+    },
 	},
 	plugins: [],
 }
